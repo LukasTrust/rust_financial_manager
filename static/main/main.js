db4ef2b6fe41a42782497f9cce3e5f9501b010d2
LukasTrust <108077617+LukasTrust@users.noreply.github.com>
2024-09-15 10:15:42 +0200

    revert

--- static/main/main.js
+++ static/main/main.js
@@ -19,7 +19,6 @@ export function error(message, context = '', ...data) {
     console.error(`[${new Date().toISOString()}] [${context}] ${message}`, ...data);
 }
 
-
 let oldUrl = localStorage.getItem('old_url') || '/dashboard';
 let release_mode = false;
 
