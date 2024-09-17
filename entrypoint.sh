e1768ea4f945f8dbc44d6986658d2eaabdb1ceba
LukasTrust <108077617+LukasTrust@users.noreply.github.com>
2024-09-17 10:22:42 +0200

    again

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,16 @@ if [ -z "$ROCKET_SECRET_KEY" ]; then
   export ENV ROCKET_SECRET_KEY=$(openssl rand -base64 32)
 fi
 
-# Load the .env file to set environment variables, including DATABASE_URL
+# Load the .env file to set environment variables
 if [ -f .env ]; then
   export $(cat .env | grep -v '^#' | xargs)
 fi
+
+# Build the DATABASE_URL dynamically
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:${POSTGRES_PORT}/${POSTGRES_DB}"
+
+# Output the DATABASE_URL for debugging
+echo "DATABASE_URL is: $DATABASE_URL"
 
 # Wait for Postgres to be ready by using the DATABASE_URL
 until diesel database reset --database-url "$DATABASE_URL"; do
